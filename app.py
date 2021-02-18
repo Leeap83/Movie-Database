fbb0e4298b9f6d1ad41337fa60533f2198229028
Leeap83 <leeap83@gmail.com>
2021-02-18 12:38:34 +0000

    Created Log in page and functionaliy

--- app.py
+++ app.py
@@ -19,6 +19,11 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/get_movies")
 def get_movies():
     movies = mongo.db.movies.find()
@@ -44,6 +49,28 @@ def register():
         session["register"] = request.form.get("username").lower()
         flash("Successfully registered, welcome!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["register"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
